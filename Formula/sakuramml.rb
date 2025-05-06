bb32387cf48b418e041801611388d11e5c4335c9
horaguy <157380662+horaguy@users.noreply.github.com>
2025-05-07 01:57:28 +0900

    Add formula for Sakuramml MML compiler

---
+++ Formula/sakuramml.rb
@@ -0,0 +1,18 @@
+class Sakuramml < Formula
+  desc "MML Compiler SAKURA Rust"
+  homepage "https://github.com/kujirahand/sakuramml-rust"
+  url "https://github.com/kujirahand/sakuramml-rust/archive/refs/tags/0.1.42.tar.gz"
+  sha256 "3426153ccc96d9af3f32d1628a6af3b67010e3f95243aedd21d4ba353a1b1526"
+  license "MIT"
+  head "https://github.com/kujirahand/sakuramml-rust.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--path", ".", "--root", prefix
+  end
+
+  test do
+    system "#{bin}/sakuramml", "--help"
+  end
+end 
