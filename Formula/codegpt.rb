6353be853f773cdd6926de7e05fe3266eafff1d3
horaguy <157380662+horaguy@users.noreply.github.com>
2025-05-20 21:41:24 +0900

    [chore]: clean up unused dependency references in the formula
    Signed-off-by: horaguy <157380662+horaguy@users.noreply.github.com>

--- Formula/codegpt.rb
+++ Formula/codegpt.rb
@@ -5,10 +5,7 @@ class Codegpt < Formula
   homepage "https://github.com/appleboy/CodeGPT"
   url "https://github.com/appleboy/CodeGPT/releases/download/v1.1.0/CodeGPT-1.1.0-darwin-arm64.xz"
   version "1.1.0"
-
   sha256 "7d29ed1b7a8cfa2ac9377a7a8cf8a2856bebbedc014c095c09894a09aae0cdce"
-  # depends_on "git"
-  # depends_on "xz"
 
   def install
     filename = "CodeGPT-#{version}-darwin-arm64"
