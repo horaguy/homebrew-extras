5364aa7cc96f7e5a9ed6a2277aa14363eb21ebb4
horaguy <157380662+horaguy@users.noreply.github.com>
2025-05-25 23:20:44 +0900

    [feat]: add Melodyne cask and update workflow to include it
    Signed-off-by: horaguy <157380662+horaguy@users.noreply.github.com>

---
+++ Casks/melodyne.rb
@@ -0,0 +1,28 @@
+cask "melodyne" do
+  version "5.4.2.006"
+  sha256 "88d31b7eadf2a2c6c3eb931ae115706c25df2750c81cfedd76840a6ce0df9327"
+
+  url "https://s3.eu-west-1.amazonaws.com/assets.celemony.com/Demos/Melodyne.#{version}-Demo.dmg"
+  name "Melodyne"
+  desc "Note-based audio editing software"
+  homepage "https://www.celemony.com/melodyne"
+
+  livecheck do
+    url "https://services.celemony.com/cgi-bin/WebObjects/LicenseApp.woa/wa/demoRegistration?item=Melodyne5-Demo-Mac&language=en"
+    regex(/Melodyne\.(\d+(?:\.\d+)*)-Demo\.dmg/)
+  end
+
+  depends_on macos: ">= :sierra"
+
+  app "Melodyne.app"
+
+  uninstall launchctl: "com.celemony.melodyne",
+            quit:      "com.celemony.melodyne"
+
+  zap trash: [
+    "~/Library/Application Support/Melodyne",
+    "~/Library/Preferences/com.celemony.melodyne.*",
+    "~/Library/Saved Application State/com.celemony.melodyne.savedState",
+    "~/Library/Logs/Melodyne",
+  ]
+end
