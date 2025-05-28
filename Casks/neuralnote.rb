31ad215c17dd0de23afcf528f0b600c917543401
horaguy <157380662+horaguy@users.noreply.github.com>
2025-05-28 23:30:48 +0900

    [fix]: correct versioning in download URL construction
    Signed-off-by: horaguy <157380662+horaguy@users.noreply.github.com>

--- Casks/neuralnote.rb
+++ Casks/neuralnote.rb
@@ -2,7 +2,7 @@ cask "neuralnote" do
   version "v1.1.0"
   sha256 "67e6d08ffd6691cdb346ec49866b3765f83725e8bbc7f8470c5d26669c98ef74"
 
-  url "https://github.com/DamRsn/NeuralNote/releases/download/#{version}/NeuralNote_Installer_Mac.pkg"
+  url "https://github.com/DamRsn/NeuralNote/releases/download/v#{version}/NeuralNote_Installer_Mac.pkg"
   name "NeuralNote"
   desc "Audio Plugin for Audio to MIDI transcription using deep learning"
   homepage "https://github.com/DamRsn/NeuralNote"
