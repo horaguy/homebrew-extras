686bf30a68c78f6b04a7c682b6b4a7112d48be57
horaguy <157380662+horaguy@users.noreply.github.com>
2025-11-21 23:54:02 +0900

    feat: Add Epic Games Launcher cask with version and auto-update support

---
+++ Casks/epic-games.rb
@@ -0,0 +1,31 @@
+cask "epic-games" do
+  version "19.0.0"
+  sha256 "78e782ff73b3ddaa267b3d536af748b460200398133527cd199562955c1f184c"
+
+  url "https://epicgames-download1.akamaized.net/Builds/UnrealEngineLauncher/Installers/Mac/EpicInstaller-#{version}.dmg",
+      verified: "epicgames-download1.akamaized.net/"
+  name "Epic Games Launcher"
+  desc "Launcher for *Epic Games* games"
+  homepage "https://www.epicgames.com/"
+
+  livecheck do
+    url "https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncher.dmg"
+    strategy :header_match
+  end
+
+  auto_updates true
+  depends_on macos: ">= :ventura"
+
+  app "Epic Games Launcher.app"
+
+  zap trash: [
+    "~/Library/Application Support/Epic",
+    "~/Library/Caches/com.epicgames.EpicGamesLauncher",
+    "~/Library/Cookies/com.epicgames.EpicGamesLauncher.binarycookies",
+    "~/Library/HTTPStorages/com.epicgames.CrashReportClient",
+    "~/Library/HTTPStorages/com.epicgames.EpicGamesLauncher",
+    "~/Library/Logs/Unreal Engine/EpicGamesLauncher",
+    "~/Library/Preferences/Unreal Engine/EpicGamesLauncher",
+  ]
+end
+
