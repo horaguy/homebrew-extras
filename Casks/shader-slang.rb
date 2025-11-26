a0cb40c7a901a543a2dcd8828c2d31fb4807650f
horaguy <157380662+horaguy@users.noreply.github.com>
2025-11-26 16:10:50 +0900

    feat: Simplify installation by replacing postflight with artifacts

--- Casks/shader-slang.rb
+++ Casks/shader-slang.rb
@@ -17,38 +17,13 @@ cask "shader-slang" do
   binary "slangc"
   binary "slangd"
 
-  postflight do
-    require "fileutils"
-
-    # Install libraries to Homebrew's lib directory
-    lib_path = "/opt/homebrew/lib"
-    FileUtils.mkdir_p lib_path
-    [
-      "libgfx.0.#{version}.dylib",
-      "libslang-compiler.0.#{version}.dylib",
-      "libslang-glsl-module-#{version}.dylib",
-      "libslang-glslang-#{version}.dylib",
-      "libslang-llvm.dylib",
-      "libslang-rt.0.#{version}.dylib",
-    ].each do |dylib|
-      FileUtils.cp staged_path/dylib, lib_path
-    end
-
-    # Install modules to Homebrew's share directory
-    share_path = "/opt/homebrew/share/shader-slang"
-    FileUtils.mkdir_p share_path
-    FileUtils.cp staged_path/"neural.slang-module", share_path
-  end
-
-  uninstall delete: [
-    "/opt/homebrew/lib/libgfx.0.#{version}.dylib",
-    "/opt/homebrew/lib/libslang-compiler.0.#{version}.dylib",
-    "/opt/homebrew/lib/libslang-glsl-module-#{version}.dylib",
-    "/opt/homebrew/lib/libslang-glslang-#{version}.dylib",
-    "/opt/homebrew/lib/libslang-llvm.dylib",
-    "/opt/homebrew/lib/libslang-rt.0.#{version}.dylib",
-    "/opt/homebrew/share/shader-slang",
-  ]
+  artifact "libgfx.0.#{version}.dylib", target: "#{HOMEBREW_PREFIX}/lib/libgfx.0.#{version}.dylib"
+  artifact "libslang-compiler.0.#{version}.dylib", target: "#{HOMEBREW_PREFIX}/lib/libslang-compiler.0.#{version}.dylib"
+  artifact "libslang-glsl-module-#{version}.dylib", target: "#{HOMEBREW_PREFIX}/lib/libslang-glsl-module-#{version}.dylib"
+  artifact "libslang-glslang-#{version}.dylib", target: "#{HOMEBREW_PREFIX}/lib/libslang-glslang-#{version}.dylib"
+  artifact "libslang-llvm.dylib", target: "#{HOMEBREW_PREFIX}/lib/libslang-llvm.dylib"
+  artifact "libslang-rt.0.#{version}.dylib", target: "#{HOMEBREW_PREFIX}/lib/libslang-rt.0.#{version}.dylib"
+  artifact "neural.slang-module", target: "#{HOMEBREW_PREFIX}/share/shader-slang/neural.slang-module"
 
   zap trash: []
 end
