670d8f881b8adcbb3d1fb95f08fb31c1e83e1b9c
horaguy <157380662+horaguy@users.noreply.github.com>
2026-02-04 16:42:36 +0900

    feat: Update Moho download URL to include the 'download' suffix

--- Casks/moho.rb
+++ Casks/moho.rb
@@ -3,7 +3,7 @@ cask "moho" do
   sha256 "abd8dd9f87c8cb1ef5fce90d393c4f7730e4c53cdfa404bef15f015c7e3e65c6"
 
   # url "https://www.dropbox.com/scl/fi/nrz0dcufujy6093mk4g2h/Moho1440_Mac.dmg?rlkey=wo8ug7lalxmjfzdmcgzrh0gyl&st=q83hblkk&dl=1"
-  url "https://delivery.shopifyapps.com/-/b1b5e7614552eeac/15449ea520c87e08",
+  url "https://delivery.shopifyapps.com/-/b1b5e7614552eeac/15449ea520c87e08/download",
     user_agent: :fake
   name "MOHO"
   desc "Vector based 2D Computer animation software"
