42d9046df98a4fd1d4d3c5343c588b340b06ea15
horaguy <157380662+horaguy@users.noreply.github.com>
2026-02-05 00:39:36 +0900

    feat: Update Moho download link and adjust zap paths for clarity

--- Casks/moho.rb
+++ Casks/moho.rb
@@ -2,11 +2,8 @@ cask "moho" do
   version "14.4"
   sha256 "abd8dd9f87c8cb1ef5fce90d393c4f7730e4c53cdfa404bef15f015c7e3e65c6"
 
-  # url "https://www.dropbox.com/scl/fi/nrz0dcufujy6093mk4g2h/Moho1440_Mac.dmg?rlkey=wo8ug7lalxmjfzdmcgzrh0gyl&st=q83hblkk&dl=1",
-  # https://github.com/Homebrew/homebrew-cask/blob/f479af4c1fa286128b3c61261edf31f2ed646ffc/Casks/c/command-x.rb#L6 ?
+  # Mirror Link (primary link is https://delivery.shopifyapps.com/-/b1b5e7614552eeac/15449ea520c87e08 but it's not work with CLI)
   url "https://dl.dropboxusercontent.com/scl/fi/nrz0dcufujy6093mk4g2h/Moho1440_Mac.dmg?rlkey=wo8ug7lalxmjfzdmcgzrh0gyl&st=q83hblkk"
-  # url "https://delivery.shopifyapps.com/-/b1b5e7614552eeac/15449ea520c87e08",
-  #   user_agent: :browser
   name "MOHO"
   desc "Vector based 2D Computer animation software"
   homepage "https://moho.lostmarble.com"
@@ -14,8 +11,7 @@ cask "moho" do
   app "Moho.app"
 
   zap trash: [
-    "~/Library/Application\ Scripts/com.lostmarble.moho.MohoThumbnailer",
-    "~/Library/Application\ Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.lostmarble.moho.sfl4",
+    "~/Library/Application Scripts/com.lostmarble.moho.MohoThumbnailer",
     "~/Library/Containers/com.lostmarble.moho.MohoThumbnailer",
     "~/Library/Preferences/com.lostmarble.moho.plist",
   ]
