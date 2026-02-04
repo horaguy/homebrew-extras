f9389fa8343c93bb38bffcecb591f8fd9cef2c3f
horaguy <157380662+horaguy@users.noreply.github.com>
2026-02-04 16:03:28 +0900

    feat: Add casks for Live2D, Moho, and Spine animation software

---
+++ Casks/live2d.rb
@@ -0,0 +1,20 @@
+cask "live2d" do
+    version "5.3.00"
+    sha256 "0292e93216e0a24ab1065c69c4ded70b81d39d9f4b24d1ccbedd2b42c819459e"
+
+    url "https://cubism.live2d.com/editor/bin/Live2D_Cubism_Setup_#{version}_arm64.pkg"
+    name "Live2D Cubism Editor"
+    desc "2D software to edit Live2D models"
+    homepage "https://www.live2d.com/"
+
+    livecheck do
+      url "https://www.live2d.com/cubism/download/editor/"
+      # <span id="ver_mac" class="btn_ver">5.3.00 (2026-01-20)</span> のエレメントからバージョン(5.3.00)を取得
+      # (idだけ指定すれば良く、classはあってもなくても良いものとする
+      regex(/<span id="ver_mac" [^>]*>(\d+(?:\.\d+)*)\s*\(.*?\)</span>/)
+    end
+
+    # pkg "Live2D_Cubism_Setup_#{version}_arm64.pkg"
+
+    # uninstall pkgutil: "com.live2d.cubism.editor"
+  end
---
+++ Casks/moho.rb
@@ -0,0 +1,18 @@
+cask "moho" do
+    version "14.4"
+    sha256 "abd8dd9f87c8cb1ef5fce90d393c4f7730e4c53cdfa404bef15f015c7e3e65c6"
+
+    url "https://www.dropbox.com/scl/fi/nrz0dcufujy6093mk4g2h/Moho1440_Mac.dmg?rlkey=wo8ug7lalxmjfzdmcgzrh0gyl&st=q83hblkk&dl=1"
+    name "MOHO"
+    desc "Vector based 2D Computer animation software"
+    homepage "https://moho.lostmarble.com"
+
+    app "Moho.app"
+
+    zap trash: [
+      "~/Library/Application\ Scripts/com.lostmarble.moho.MohoThumbnailer",
+      "~/Library/Application\ Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.lostmarble.moho.sfl4",
+      "~/Library/Containers/com.lostmarble.moho.MohoThumbnailer",
+      "~/Library/Preferences/com.lostmarble.moho.plist",
+    ]
+  end
