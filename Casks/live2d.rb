53133c08a82dc0027bf612769d4757093639a1bd
horaguy <157380662+horaguy@users.noreply.github.com>
2026-02-04 16:38:39 +0900

    feat: Format the live2d and moho casks for consistency and clarity

--- Casks/live2d.rb
+++ Casks/live2d.rb
@@ -1,18 +1,18 @@
 cask "live2d" do
-    version "5.3.00"
-    sha256 "0292e93216e0a24ab1065c69c4ded70b81d39d9f4b24d1ccbedd2b42c819459e"
+  version "5.3.00"
+  sha256 "0292e93216e0a24ab1065c69c4ded70b81d39d9f4b24d1ccbedd2b42c819459e"
 
-    url "https://cubism.live2d.com/editor/bin/Live2D_Cubism_Setup_#{version}_arm64.pkg"
-    name "Live2D Cubism Editor"
-    desc "2D software to edit Live2D models"
-    homepage "https://www.live2d.com/"
+  url "https://cubism.live2d.com/editor/bin/Live2D_Cubism_Setup_#{version}_arm64.pkg"
+  name "Live2D Cubism Editor"
+  desc "2D software to edit Live2D models"
+  homepage "https://www.live2d.com/"
 
-    livecheck do
-      url "https://www.live2d.com/cubism/download/editor/"
-      regex(/<span id="ver_mac" [^>]*>(\d+(?:\.\d+)*)\s*\(.*?\)<\/span>/)
-    end
-
-    pkg "Live2D_Cubism_Setup_#{version}_arm64.pkg"
-
-    uninstall delete: "/Applications/Live2D Cubism *"
+  livecheck do
+    url "https://www.live2d.com/cubism/download/editor/"
+    regex(/<span id="ver_mac" [^>]*>(\d+(?:\.\d+)*)\s*\(.*?\)<\/span>/)
   end
+
+  pkg "Live2D_Cubism_Setup_#{version}_arm64.pkg"
+
+  uninstall delete: "/Applications/Live2D Cubism *"
+end
--- Casks/moho.rb
+++ Casks/moho.rb
@@ -1,19 +1,20 @@
 cask "moho" do
-    version "14.4"
-    sha256 "abd8dd9f87c8cb1ef5fce90d393c4f7730e4c53cdfa404bef15f015c7e3e65c6"
+  version "14.4"
+  sha256 "abd8dd9f87c8cb1ef5fce90d393c4f7730e4c53cdfa404bef15f015c7e3e65c6"
 
-    # url "https://www.dropbox.com/scl/fi/nrz0dcufujy6093mk4g2h/Moho1440_Mac.dmg?rlkey=wo8ug7lalxmjfzdmcgzrh0gyl&st=q83hblkk&dl=1"
-    url "https://delivery.shopifyapps.com/-/b1b5e7614552eeac/15449ea520c87e08"
-    name "MOHO"
-    desc "Vector based 2D Computer animation software"
-    homepage "https://moho.lostmarble.com"
+  # url "https://www.dropbox.com/scl/fi/nrz0dcufujy6093mk4g2h/Moho1440_Mac.dmg?rlkey=wo8ug7lalxmjfzdmcgzrh0gyl&st=q83hblkk&dl=1"
+  url "https://delivery.shopifyapps.com/-/b1b5e7614552eeac/15449ea520c87e08",
+      user_agent: :fake,
+  name "MOHO"
+  desc "Vector based 2D Computer animation software"
+  homepage "https://moho.lostmarble.com"
 
-    app "Moho.app"
+  app "Moho.app"
 
-    zap trash: [
-      "~/Library/Application\ Scripts/com.lostmarble.moho.MohoThumbnailer",
-      "~/Library/Application\ Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.lostmarble.moho.sfl4",
-      "~/Library/Containers/com.lostmarble.moho.MohoThumbnailer",
-      "~/Library/Preferences/com.lostmarble.moho.plist",
-    ]
-  end
+  zap trash: [
+    "~/Library/Application\ Scripts/com.lostmarble.moho.MohoThumbnailer",
+    "~/Library/Application\ Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.lostmarble.moho.sfl4",
+    "~/Library/Containers/com.lostmarble.moho.MohoThumbnailer",
+    "~/Library/Preferences/com.lostmarble.moho.plist",
+  ]
+end
