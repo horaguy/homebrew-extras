079ae598a1ae68c3a985d8c62cd792a39554ca29
horaguy <157380662+horaguy@users.noreply.github.com>
2026-03-16 17:12:45 +0900

    feat: Add headers to the Realforce cask for improved download links

--- Casks/realforce.rb
+++ Casks/realforce.rb
@@ -5,7 +5,10 @@ cask "realforce" do
     sha256 "b05654444e7d73b01c8b22e8dbe5b71bf98c8c1a9d0f0122a38134256666de19"
 
     url "https://www.realforce.co.jp/support/download/software/mac/files/REALFORCE%20CONNECT%20Software_#{version.dots_to_hyphens}.pkg",
-        user_agent: :browser
+        user_agent: :browser,
+        headers: {
+            "Referer" => "https://www.realforce.co.jp/support/download/software/",
+        }
     name "REALFORCE for Mac"
     desc "Software for Realforce keyboards and mice"
     homepage "https://www.realforce.co.jp/"
