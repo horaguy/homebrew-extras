9826e4db6603b6e814c90abe1d1ad9ded2d0c47b
horaguy <157380662+horaguy@users.noreply.github.com>
2026-03-16 17:09:38 +0900

    feat: Update Realforce cask to use a fake user agent in the URL

--- Casks/realforce.rb
+++ Casks/realforce.rb
@@ -4,7 +4,8 @@ cask "realforce" do
     version "4.1.1"
     sha256 "b05654444e7d73b01c8b22e8dbe5b71bf98c8c1a9d0f0122a38134256666de19"
 
-    url "https://www.realforce.co.jp/support/download/software/mac/files/REALFORCE%20CONNECT%20Software_#{version.dots_to_hyphens}.pkg"
+    url "https://www.realforce.co.jp/support/download/software/mac/files/REALFORCE%20CONNECT%20Software_#{version.dots_to_hyphens}.pkg",
+        user_agent: :fake
     name "REALFORCE for Mac"
     desc "Software for Realforce keyboards and mice"
     homepage "https://www.realforce.co.jp/"
