e663f8a1531e0dc494c1f89aebc389439859d762
horaguy <157380662+horaguy@users.noreply.github.com>
2026-04-05 02:45:58 +0900

    feat: Add Aseprite cask for version 1.3.17 with release details

---
+++ Casks/aseprite.rb
@@ -0,0 +1,21 @@
+cask "aseprite" do
+  version "1.3.17"
+  sha256 "9c14489e8ef41341d740439a35382236320cf111e1ecb7fd9af84a2fe552f20c"
+
+  url "https://github.com/horaguy/aseprite-build/releases/download/v#{version}/Aseprite-v#{version}-macOS.zip",
+      header: "Authorization: token #{ENV.fetch("HOMEBREW_GITHUB_API_TOKEN")}"
+
+  name "Aseprite"
+  desc "Animated sprite editor & pixel art tool"
+  homepage "https://www.aseprite.org/"
+
+  livecheck do
+    url "https://api.github.com/repos/horaguy/aseprite-build/releases/latest"
+    regex(/v?(\d+(?:\.\d+)+)/i)
+    strategy :json do |json, regex|
+      json["tag_name"]&.scan(regex)&.flatten
+    end
+  end
+
+  app "Aseprite.app"
+end
