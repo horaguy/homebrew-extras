59c05f15b73e6ae2556dbc4d8476208986928e5e
horaguy <157380662+horaguy@users.noreply.github.com>
2026-04-05 12:56:56 +0900

    feat: Update Aseprite cask to use array for headers in URL request

--- Casks/aseprite.rb
+++ Casks/aseprite.rb
@@ -1,14 +1,16 @@
 cask "aseprite" do
   version "1.3.17"
-  sha256 "9c14489e8ef41341d740439a35382236320cf111e1ecb7fd9af84a2fe552f20c"
+  sha256 "54edab654452a2797ae5f701a91956c02f72e57f88a89a63ae282692d67330ba"
 
   # url "https://github.com/horaguy/aseprite-build/releases/download/v#{version}/Aseprite-v#{version}-macOS.zip",
   #     header: "Authorization: token #{ENV.fetch("HOMEBREW_GITHUB_API_TOKEN")}"
-  url "https://#{ENV.fetch("HOMEBREW_GITHUB_API_TOKEN")}@api.github.com/repos/horaguy/aseprite-build/releases/assets/388832112",
-    header: "Accept: application/octet-stream"
-      # "Authorization: token #{ENV.fetch("HOMEBREW_GITHUB_API_TOKEN")}",
-
-  
+  # url "https://#{ENV.fetch("HOMEBREW_GITHUB_API_TOKEN")}@api.github.com/repos/horaguy/aseprite-build/releases/assets/388832112",
+  #   header: "Accept: application/octet-stream"
+  url "https://api.github.com/repos/horaguy/aseprite-build/releases/assets/388832112",
+    header: [
+      "Authorization: token #{ENV.fetch("HOMEBREW_GITHUB_API_TOKEN")}",
+      "Accept: application/octet-stream",
+    ]
 
   name "Aseprite"
   desc "Animated sprite editor & pixel art tool"
