ec5dd407419ac14dc5bc0cebd298edfce273d84b
horaguy <157380662+horaguy@users.noreply.github.com>
2026-04-06 01:00:57 +0900

    feat: Refactor bump script to streamline version checking and bumping

--- .github/scripts/bump.sh
+++ .github/scripts/bump.sh
@@ -5,15 +5,8 @@
 FULL_PKG_NAME="$(brew tap)/$1"
 CASK_FLAG=$2
 
-# Get current and latest version from livecheck
-brew livecheck "$FULL_PKG_NAME" # To remove extra log from json, pre-run same command.
-brew livecheck --quiet --json --newer-only "$FULL_PKG_NAME"
-CURRENT_VERSION=$(echo "$JSON" | jq -r '.[0].version.current')
-LATEST_VERSION=$(echo "$JSON" | jq -r '.[0].version.latest')
-# if livecheck skipped, CURRENT_VERSION and LATEST_VERSION is "null"
-[ -z "$CURRENT_VERSION" ] && echo "Current version is not found" && exit 1
-[ -z "$LATEST_VERSION" ] && echo "Latest version is not found" && exit 1
-if [ "$CURRENT_VERSION" = "$LATEST_VERSION" ]; then
+LATEST_VERSION=$(brew livecheck --quiet --json --newer-only "$FULL_PKG_NAME" | jq -r '.[0].version.latest // empty')
+if [ -z "$LATEST_VERSION" ]; then
   echo "$FULL_PKG_NAME is already up to date (or livecheck is skipped)"
   exit 0
 fi
